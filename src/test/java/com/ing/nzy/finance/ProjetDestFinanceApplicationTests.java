85a6ddad55a17cef1a51bf81286e4f0adfb1e25a
YASSINE <ouakliyassine98@gmail.com>
2022-01-15 17:10:14 +0100

    initial commit

---
+++ src/test/java/com/ing/nzy/finance/ProjetDestFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.nzy.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDestFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
